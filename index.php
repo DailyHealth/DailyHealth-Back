c267956867007c3e42300d28577386e7b9bef11c
CorentinS <44869996+CorentinSETTELEN@users.noreply.github.com>
2020-04-14 13:06:17 +0200

    Test front - back connexion

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/*header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");*/
+
+
+echo '<pre>'; print_r( $_POST ); echo '</pre>';
+echo '<pre>'; print_r( $_REQUEST ); echo '</pre>';
+
+if( ! empty($_POST["action"]) ){
+  echo "Contenu de action : " . $_POST["action"];
+
+}else{
+  echo "Requete vide";
+}
+
+
+
+?>
